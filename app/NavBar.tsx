81e02357269cca8efaa3277d89ccc78366d0261c
Xingdi Tan <steventanxd@gmail.com>
2023-12-23 21:41:43 -0800

    style th eactive link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaBug } from "react-icons/fa";
+import classnames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
@@ -15,7 +19,11 @@ const NavBar = () => {
       <ul className="flex space-x-6">
         {links.map(({ href, label }) => (
           <Link
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": href === currentPath,
+              "text-zinc-500": href !== currentPath,
+              "hover:text-zinc-800": true,
+            })}
             href={href}
           >
             {label}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import Image from "next/image";
-import NavBar from "./NavBar";
-
 export default function Home() {
   return (
     <>
-      <NavBar></NavBar>
       <main>Hello World</main>
     </>
   );
