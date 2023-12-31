e2c857fd8a48540ff203d3757aa957275bf59631
Xingdi Tan <steventanxd@gmail.com>
2023-12-30 22:28:12 -0800

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,5 @@
 import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
+import Link from "../components/Link";
 import React from "react";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
