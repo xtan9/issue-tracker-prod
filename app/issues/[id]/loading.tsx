e2cff7a4c91042822cf37cca511d27d9be256692
Xingdi Tan <steventanxd@gmail.com>
2023-12-30 21:20:33 -0800

    show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssueDetailLoadingPage = () => {
+  return <div>IssueDetailLoadingPage</div>;
+};
+
+export default IssueDetailLoadingPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -8,7 +8,7 @@ import delay from "delay";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(1000);
+  await delay(700);
   return (
     <div>
       <IssueActions />
@@ -29,7 +29,7 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="md:hidden">
                   <IssueStatusBadge status={issue.status} />
                 </div>
