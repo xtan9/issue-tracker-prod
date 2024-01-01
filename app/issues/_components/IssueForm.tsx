9f159cc8d659e90c5d40843ee08f211c369bcc9e
Xingdi Tan <steventanxd@gmail.com>
2023-12-31 19:06:56 -0800

    fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -47,6 +47,7 @@ const IssueForm = ({ issue }: Props) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setError("An unexpected error occured.");
     } finally {
