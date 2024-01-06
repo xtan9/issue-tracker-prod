66a3c03fe52c0de17f01667e917c8aa8aa2e1c3c
Xingdi Tan <steventanxd@gmail.com>
2024-01-05 22:39:22 -0800

    refactor the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,77 +1,25 @@
 "use client";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import React from "react";
-import { FaBug } from "react-icons/fa";
-import classnames from "classnames";
-import { useSession } from "next-auth/react";
 import {
   Avatar,
   Box,
   Container,
   DropdownMenu,
-  DropdownMenuTrigger,
   Flex,
   Text,
 } from "@radix-ui/themes";
+import classnames from "classnames";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
-  const currentPath = usePathname();
-  const { status, data: session } = useSession();
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" },
-  ];
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
         <Flex justify="between">
-          <Flex align="center" gap="3">
-            <Link href="/">
-              <FaBug />
-            </Link>
-            <ul className="flex space-x-6">
-              {links.map(({ href, label }) => (
-                <li key={href}>
-                  <Link
-                    className={classnames({
-                      "text-zinc-900": href === currentPath,
-                      "text-zinc-500": href !== currentPath,
-                      "hover:text-zinc-800": true,
-                    })}
-                    href={href}
-                  >
-                    {label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </Flex>
-          <Box>
-            {status === "authenticated" && (
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    className="cursor-pointer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <DropdownMenu.Label>
-                    <Text size="2">{session.user!.email}</Text>
-                  </DropdownMenu.Label>
-                  <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Log out</Link>
-                  </DropdownMenu.Item>
-                </DropdownMenu.Content>
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Login</Link>
-            )}
-          </Box>
+          <NavLinks />
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
@@ -79,3 +27,70 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  if (status === "authenticated")
+    return (
+      <Box>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Avatar
+              src={session.user!.image!}
+              fallback="?"
+              size="2"
+              className="cursor-pointer"
+            />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Label>
+              <Text size="2">{session.user!.email}</Text>
+            </DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Log out</Link>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Box>
+    );
+  if (status === "unauthenticated")
+    return (
+      <Box>
+        <Link className="nav-link" href="/api/auth/signin">
+          Login
+        </Link>
+      </Box>
+    );
+  return null;
+};
+
+const NavLinks = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <Flex align="center" gap="3">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(({ href, label }) => (
+          <li key={href}>
+            <Link
+              className={classnames({
+                "nav-link": true,
+                "!text-zinc-900": href === currentPath,
+              })}
+              href={href}
+            >
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Flex>
+  );
+};
