f3243002477025bbd48519bccda66fe05d783770
Xingdi Tan <steventanxd@gmail.com>
2024-01-07 17:58:24 -0800

    Use Tanstack Query and Add Assignee to Issue Table

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,23 +1,32 @@
 "use client";
 import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Skeleton } from "@/app/components";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/xapi/users").then((res) => res.data),
+    staleTime: 10 * 1000, //60s
+    retry: 3,
+  });
 
-  useEffect(() => {
-    axios.get<User[]>("/api/users").then((res) => {
-      setUsers(res.data);
-    });
-  }, []);
+  if (isLoading) return <Skeleton />;
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign to..." />
       <Select.Content>
         <Select.Group>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,16 @@
-export { default } from "next-auth/middleware";
+// see https://stackoverflow.com/questions/72807218/next-auth-js-with-next-js-middleware-redirects-to-sign-in-page-after-successful#:~:text=I%20manage%20it%20like%20this
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  callbacks: {
+    authorized: ({ req }) => {
+      // verify token and return a boolean
+      const sessionToken = req.cookies.get("next-auth.session-token");
+      if (sessionToken) return true;
+      else return false;
+    },
+  },
+});
 
 export const config = {
   matcher: ["/issues/new", "/issues/edit/:id+"],
